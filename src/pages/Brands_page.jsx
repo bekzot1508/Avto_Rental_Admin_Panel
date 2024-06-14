c7dd94f5b9fdd0e57d7a9c25130f67cc75b69002
umid2401 <umamatraximov@gmail.com>
2024-06-15 00:21:48 +0500

    new changes

--- src/pages/Brands_page.jsx
+++ src/pages/Brands_page.jsx
@@ -4,7 +4,6 @@ const Brands_page = () => {
   return (
     <div>
       <h2>Hello, My friends</h2>
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, animi?</h3>
     </div>
   )
 }
