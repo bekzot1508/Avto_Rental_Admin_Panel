7d27a4259d9d7b4ad045f93e84edf7434a538a1f
umid2401 <umamatraximov@gmail.com>
2024-06-19 18:51:27 +0500

    Tasks is done

--- src/pages/Brands_page.jsx
+++ src/pages/Brands_page.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Brand from '/src/components/Brand/Brand'
+import Brand from '/src/components/Brands/Brand'
 
 export default function Brands_page() {
   return (
